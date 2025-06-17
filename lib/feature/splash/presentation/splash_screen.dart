65c04350f9175527db9c30217e31305dd5050494
ALHassan Musleh <80463251+ALHassanMusleh@users.noreply.github.com>
2025-06-17 07:32:14 +0300

    feat(splash&onboarding): - do testing

--- lib/feature/splash/presentation/splash_screen.dart
+++ lib/feature/splash/presentation/splash_screen.dart
@@ -1,17 +1,20 @@
 import 'package:fitness_app/core/common/height_width_extention.dart';
 import 'package:fitness_app/core/router/pages_routes.dart';
+import 'package:fitness_app/core/services/screen_size_service.dart';
 import 'package:fitness_app/core/services/shared_preference_services.dart';
 import 'package:fitness_app/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+ const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+
   @override
   void initState() {
     _executeNavigation();
@@ -34,6 +37,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenSizeService.init(context);
     return Scaffold(
       body: Center(
         child: Image.asset(
