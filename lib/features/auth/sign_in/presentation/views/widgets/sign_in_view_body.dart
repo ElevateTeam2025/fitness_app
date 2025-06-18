c6dbdaa9385966b504ce7d4957d3e980ff557362
Feto505 <177836466+Feto505@users.noreply.github.com>
2025-06-18 21:31:48 +0300

    refactor(ref-login): edit sign in form to separate Field validation.

--- lib/features/auth/sign_in/presentation/views/widgets/sign_in_view_body.dart
+++ lib/features/auth/sign_in/presentation/views/widgets/sign_in_view_body.dart
@@ -5,12 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
+import '../../../../../../core/widgets/custom_validate.dart';
 import '../../../domain/entity/sign_in_request.dart';
 import '../../cubit/sign_in_cubit/sign_in_state.dart';
 import '../../cubit/sign_in_cubit/sign_in_view_model.dart';
 import 'sign_in_form.dart';
 import 'sign_in_header.dart';
-
 
 class SignInViewBody extends StatefulWidget {
   final SignInViewModel signInViewModel;
@@ -25,26 +25,38 @@ class _SignInViewBodyState extends State<SignInViewBody> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  AutovalidateMode validateMode = AutovalidateMode.disabled;
   bool isPasswordVisible = false;
+  bool isFormValid = false;
+
+  late FocusNode emailFocusNode;
+  late FocusNode passwordFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    emailFocusNode = FocusNode()..addListener(() => setState(() {}));
+    passwordFocusNode = FocusNode()..addListener(() => setState(() {}));
+
+    _emailController.addListener(validateForm);
+    _passwordController.addListener(validateForm);
+  }
 
   @override
   void dispose() {
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
   }
 
-  void onChange(value) {
-    if (_formKey.currentState!.validate()) {
-      setState(() {
-        validateMode = AutovalidateMode.disabled;
-      });
-    } else {
-      setState(() {
-        validateMode = AutovalidateMode.always;
-      });
-    }
+  void validateForm() {
+    final emailValid = AppValidate.validateEmail(_emailController.text) == null;
+    final passwordValid = AppValidate.validatePassword(_passwordController.text) == null;
+
+    setState(() {
+      isFormValid = emailValid && passwordValid;
+    });
   }
 
   @override
@@ -56,7 +68,7 @@ class _SignInViewBodyState extends State<SignInViewBody> {
           EasyLoading.show();
         } else if (state is SuccessSignInState) {
           EasyLoading.dismiss();
-          EasyLoading.showSuccess('success');
+          EasyLoading.showSuccess('Success');
           Navigator.pushReplacementNamed(context, PagesRoutes.layoutView);
         } else if (state is ErrorSignInState) {
           EasyLoading.dismiss();
@@ -66,12 +78,8 @@ class _SignInViewBodyState extends State<SignInViewBody> {
       builder: (context, state) {
         return SafeArea(
           child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(
-              // horizontal: responsiveWidth(16),
-              vertical: responsiveHeight(32),
-            ),
+            padding: EdgeInsets.symmetric(vertical: responsiveHeight(32)),
             child: Form(
-              autovalidateMode: validateMode,
               key: _formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -83,30 +91,24 @@ class _SignInViewBodyState extends State<SignInViewBody> {
                       SignInForm(
                         emailController: _emailController,
                         passwordController: _passwordController,
-                        validateMode: validateMode,
+                        emailFocusNode: emailFocusNode,
+                        passwordFocusNode: passwordFocusNode,
                         isPasswordVisible: isPasswordVisible,
+                        isFormValid: isFormValid,
                         onPasswordVisibilityToggle: () {
                           setState(() {
                             isPasswordVisible = !isPasswordVisible;
                           });
                         },
-                        onChange: onChange,
                         onSubmit: () {
-                          if (validateMode == AutovalidateMode.always) return;
-
-                          if (_formKey.currentState!.validate()) {
-                            setState(() {
-                              validateMode = AutovalidateMode.disabled;
-                            });
-                            var data = SignInRequest(
+                          final isValid = _formKey.currentState?.validate() ?? false;
+                          if (isValid) {
+                            final data = SignInRequest(
                               email: _emailController.text,
                               password: _passwordController.text,
                             );
-                            widget.signInViewModel.doIntent(SignInClickedIntent(data));
-                          } else {
-                            setState(() {
-                              validateMode = AutovalidateMode.always;
-                            });
+                            widget.signInViewModel
+                                .doIntent(SignInClickedIntent(data));
                           }
                         },
                       ),
