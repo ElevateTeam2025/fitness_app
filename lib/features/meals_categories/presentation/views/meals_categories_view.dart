21995d3879b801960249d23ec67dae74f52b450b
omarelsayed <omarelsayd445@gmail.com>
2025-06-20 20:00:34 +0300

    (meals categroies): finish UI of getting meal by category part 2

--- lib/features/meals_categories/presentation/views/meals_categories_view.dart
+++ lib/features/meals_categories/presentation/views/meals_categories_view.dart
@@ -1,5 +1,4 @@
 import 'package:fitness_app/core/di/injectable_initializer.dart';
-import 'package:fitness_app/features/meals_categories/domain/repo/get_meals_categories_repo.dart';
 import 'package:fitness_app/features/meals_categories/presentation/cubits/get_meals_by_category_cubit/get_meals_by_category_cubit.dart';
 import 'package:fitness_app/features/meals_categories/presentation/cubits/get_meals_categories_cubit/get_meals_categories_cubit.dart';
 import 'package:fitness_app/features/meals_categories/presentation/views/widgets/meals_categories_views_body.dart';
--- lib/features/meals_categories/presentation/views/widgets/meals_categories_views_body.dart
+++ lib/features/meals_categories/presentation/views/widgets/meals_categories_views_body.dart
@@ -65,7 +65,7 @@ class _MealsCategoriesViewsBodyState extends State<MealsCategoriesViewsBody> {
                             getMealsByCategoryViewModel,
                       ),
                     ),
-                    MealsByCategoryGridView(),
+                   MealsByCategoryGridView()
                   ],
                 ),
               ),
