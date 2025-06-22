ed3bb6bfcd234e53d54efad086455b75f378c4ae
ALHassan Musleh <80463251+ALHassanMusleh@users.noreply.github.com>
2025-06-22 14:20:26 +0300

    feat(meal-details):   - create get meal details cubit and states.

---
+++ lib/features/meal_details/presentation/cubits/get_meal_details_cubit/get_meal_details_cubit.dart
@@ -0,0 +1,40 @@
+import 'package:bloc/bloc.dart';
+import 'package:fitness_app/core/common/result.dart';
+import 'package:fitness_app/features/meal_details/domain/use_case/get_meal_details_use_case.dart';
+import 'package:fitness_app/features/meal_details/presentation/cubits/get_meal_details_cubit/get_meal_details_states.dart';
+
+class GetMealDetailsCubit extends Cubit<GetMealDetailsStates> {
+  final GetMealDetailsUseCase _getMealDetailsUseCase;
+  GetMealDetailsCubit(this._getMealDetailsUseCase)
+    : super(GetMealDetailsInitialState());
+
+  doIntents(GetMealDetailsIntent intent, {required String mealId}) async {
+    switch (intent) {
+      case OnClickGetMealDetailsIntent():
+        await _getMealDetails(mealId);
+        break;
+    }
+  }
+
+  Future<void> _getMealDetails(String id) async {
+    emit(GetMealDetailsInitialState());
+    final result = await _getMealDetailsUseCase.execute(id);
+    switch (result) {
+      case Success():
+        final data = result.data?.meals ?? [];
+        emit(GetMealDetailsSuccessState(data[0]));
+        break;
+      case Error():
+        emit(
+          GetMealDetailsErrorState(
+            message: result.exception ?? 'An error occurred',
+          ),
+        );
+        break;
+    }
+  }
+}
+
+sealed class GetMealDetailsIntent {}
+
+class OnClickGetMealDetailsIntent extends GetMealDetailsIntent {}
